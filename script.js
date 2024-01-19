c51c88004c00f607dc906c37011662c2cadae7e3
antoino filoho <antoniofilhodaconceicaorozafil@gmail.com>
2024-01-18 23:08:21 -0300

    alteraçao no index.html

---
+++ script.js
@@ -0,0 +1,17 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.sectio-top', {
+    duration: 2000,
+    distance: '80px'
+})
+
+window.addEventListener('scroll', function() {
+    var minhaSectio = document.querySelector('.minha-sectio');
+    var scrollPos = window.scrollY;
+
+    if (scrollPos > 0) {
+        minhaSectio.classList.add('scroll');
+    } else {
+        minhaSectio.classList.remove('scroll');
+    }
+});
